eeffd821cd22b59a44abae793a9e2381214f0e8b
Javier Cardona <javier.cardona@gmail.com>
2021-08-23 19:48:04 +0100

    Use BTreeMap instead of HashMap for notemap
    This has the advantage of producing a notemap.json sorted by keys (fingerings)
    so different versions can be diffed.

--- src/notemap.rs
+++ src/notemap.rs
@@ -1,17 +1,21 @@
 extern crate serde_json;
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs;
 
 const FILENAME: &str = "notemap.json";
 
-pub fn generate() -> HashMap<u32, i32> {
-    let mapfile = fs::read_to_string(FILENAME).expect("Something went wrong reading the file");
-    let notemap: HashMap<u32, i32> = serde_json::from_str(&mapfile).unwrap();
+pub fn generate() -> BTreeMap<u32, i32> {
+    let mapfile = fs::read_to_string(FILENAME);
+    let mapfile = match mapfile {
+        Ok(contents) => contents,
+        Err(_error) => String::from("{}"),
+    };
+    let notemap: BTreeMap<u32, i32> = serde_json::from_str(&mapfile).unwrap();
     notemap
 }
 
-pub fn save(notemap: &HashMap<u32, i32>) {
+pub fn save(notemap: &BTreeMap<u32, i32>) {
     let notemap_json = serde_json::to_string_pretty(&notemap).unwrap();
     fs::write(FILENAME, notemap_json).expect("Unable to write file");
 }
