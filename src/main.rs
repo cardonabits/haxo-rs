a43b0688f1800365cf7653b7ec1d23848cf5316f
Javier Cardona <javier.cardona@gmail.com>
2021-08-29 11:30:23 +0100

    Change midi bank number by using negative breath pressure
    Still very crude implementation of control commands, this is an early prototype.
    Draw air from mouthpiece while holding a "Low Bb" to arm control command.
    Then draw air on either "Low G" or "Low F" fingering to increase or decrease
    the bank number.  Provide auditive feedback once the command has been executed.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ extern crate time;
 extern crate static_assertions;
 extern crate structopt;
 
-use log::{debug, log_enabled, Level};
+use log::{debug, info, log_enabled, Level};
 
-use std::cmp::max;
+use std::cmp::{max, min};
 use std::error::Error;
 use std::process::Command;
 use std::thread;
@@ -49,6 +49,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     debug!("{:?}", opt);
 
     let (synth, _settings, _adriver) = synth::try_init(&opt.sf2_file, opt.bank_number);
+    let mut current_bank = opt.bank_number;
 
     println!("Starting haxophone...");
 
@@ -61,6 +62,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     let mut last_note = 0;
+    let mut control_command = false;
     loop {
         thread::sleep(Duration::from_millis(50));
 
@@ -94,6 +96,43 @@ fn main() -> Result<(), Box<dyn Error>> {
             if vol <= 0 && last_note > 0 {
                 synth.noteoff(0, last_note);
                 last_note = 0;
+            }
+
+            // Control commands
+            if pressure < -10 {
+                match midinotes::get_name(note) {
+                    Some("Low Bb") => {
+                        control_command = true;
+                        info!("Prepared to receive for control command");
+                    }
+                    Some("Low F") => {
+                        if control_command {
+                            control_command = false;
+                            current_bank = max(0, current_bank - 1);
+                            synth.program_change(0, current_bank);
+                            info!("New MIDI bank number {}", current_bank);
+                            synth.noteon(0, 51, 127);
+                            synth.cc(0, MIDI_CC_VOLUME, 127);
+                            thread::sleep(Duration::from_millis(100));
+                            synth.noteoff(0, 51);
+                        }
+                    }
+                    Some("Low G") => {
+                        if control_command {
+                            control_command = false;
+                            current_bank = min(128, current_bank + 1);
+                            synth.program_change(0, current_bank);
+                            info!("New MIDI bank number {}", current_bank);
+                            synth.noteon(0, 53, 127);
+                            synth.cc(0, MIDI_CC_VOLUME, 127);
+                            thread::sleep(Duration::from_millis(100));
+                            synth.noteoff(0, 53);
+                        }
+                    }
+                    _ => {
+                        control_command = false;
+                    }
+                }
             }
         }
     }
