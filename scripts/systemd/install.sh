2998d0974f103d003445b3b3e02ad7cf672029d4
Javier Cardona <javier.cardona@gmail.com>
2023-08-06 20:55:48 +0100

    Update service install script

--- scripts/systemd/install.sh
+++ scripts/systemd/install.sh
@@ -3,6 +3,8 @@
 SERVICE=haxo
 
 cp ${SERVICE}.service /etc/systemd/system
+cp blink-zero.service /etc/systemd/system
 mkdir -p /usr/share/haxo
 cp ../../notemap.json /usr/share/haxo
 systemctl enable ${SERVICE}
+systemctl enable blink-zero.service
