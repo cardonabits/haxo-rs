dbf0ccd2fbf898ce23e8fcfad5f041a086ece665
Javier Cardona <javier@cardonabits.com>
2023-10-08 19:59:21 +0100

    Copy startup midi file in install.sh

--- scripts/systemd/install.sh
+++ scripts/systemd/install.sh
@@ -6,5 +6,6 @@ cp ${SERVICE}.service /etc/systemd/system
 cp blink-zero.service /etc/systemd/system
 mkdir -p /usr/share/haxo
 cp ../../notemap.json /usr/share/haxo
+cp ../../midi/startup/Startup_Haxophone.mid /usr/share/haxo
 systemctl enable ${SERVICE}
 systemctl enable blink-zero.service
