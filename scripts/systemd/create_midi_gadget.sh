6461b0060451891e388791bad507bf3999dbdbc4
Javier Cardona <javier@cardonabits.com>
2023-12-21 19:38:54 +0000

    Script to great a usb_gadget midi device
    Uses our very own https://pid.codes/1209/2BAD/

---
+++ scripts/systemd/create_midi_gadget.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+modprobe libcomposite
+mkdir /sys/kernel/config/usb_gadget/midi
+cd /sys/kernel/config/usb_gadget/midi
+mkdir configs/c.1
+mkdir functions/midi.usb0
+mkdir strings/0x409
+mkdir configs/c.1/strings/0x409
+echo 0x2BAD > idProduct
+echo 0x1209 > idVendor
+echo 0001 > strings/0x409/serialnumber
+echo CardonaBits > strings/0x409/manufacturer
+echo Haxophone > strings/0x409/product
+echo "Conf 1" > configs/c.1/strings/0x409/configuration
+echo 120 > configs/c.1/MaxPower
+ln -s functions/midi.usb0 configs/c.1
+echo $(ls /sys/class/udc) > UDC
--- scripts/systemd/install.sh
+++ scripts/systemd/install.sh
@@ -7,6 +7,6 @@ cp blink-zero.service /etc/systemd/system
 mkdir -p /usr/share/haxo
 cp ../../notemap.json /usr/share/haxo
 cp ../../midi/startup/Startup_Haxophone.mid /usr/share/haxo
-grep g_midi /etc/modules || echo g_midi >> /etc/modules
+cp create_midi_gadget.sh /usr/local/bin
 systemctl enable ${SERVICE}
 systemctl enable blink-zero.service
