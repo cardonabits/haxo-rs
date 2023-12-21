7be80470543f8db5e20bde7c8a431e135d20acad
Javier Cardona <javier.cardona@gmail.com>
2023-12-21 14:47:21 +0000

    Load g_midi kernel module on boot

--- scripts/systemd/install.sh
+++ scripts/systemd/install.sh
@@ -7,5 +7,6 @@ cp blink-zero.service /etc/systemd/system
 mkdir -p /usr/share/haxo
 cp ../../notemap.json /usr/share/haxo
 cp ../../midi/startup/Startup_Haxophone.mid /usr/share/haxo
+grep g_midi /etc/modules || echo g_midi >> /etc/modules
 systemctl enable ${SERVICE}
 systemctl enable blink-zero.service
